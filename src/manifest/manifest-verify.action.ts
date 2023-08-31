2453f3df229a7e2d3b52beb8544476f82f3b2d25
Przemysław Grzywacz <przemyslaw.grzywacz@golem.network>
2023-08-31 11:51:19 +0200

    feat: verify manifest signature command JST-296

---
+++ src/manifest/manifest-verify.action.ts
@@ -0,0 +1,45 @@
+import { readManifest } from "./manifest-utils";
+import { ManifestVerifyOptions } from "./manifest-verify.options";
+
+import {X509Certificate} from 'node:crypto';
+import { readFile } from "fs/promises";
+import { createVerify } from "crypto";
+
+export async function manifestVerifyAction(options: ManifestVerifyOptions) {
+  // Read and validate the manifest.
+  await readManifest(options.manifest);
+
+  // Read manifest buffer.
+  const manifestBuffer = await readFile(options.manifest);
+  const manifestBase64 = manifestBuffer.toString('base64');
+  const certFile = Buffer.from(await readFile(options.certificate, 'ascii'), 'base64');
+  const signature = Buffer.from(await readFile(options.signature, 'ascii'), 'base64');
+  // const signature = await readFile(options.signature, 'ascii');
+
+  // FIXME: Find better way to get the second (or last?) certificate.
+  const certInput = certFile.toString();
+  const i = certInput.indexOf('-----BEGIN CERTIFICATE-----', 2);
+  const certSecond = certInput.substring(i);
+
+  // const cert = new X509Certificate(certFile);
+  const cert = new X509Certificate(Buffer.from(certSecond));
+
+  const verify = createVerify('RSA-SHA256');
+  verify.update(manifestBase64);
+  verify.update('\n'); // FIXME: Remove this, this is only for testing.
+
+  if (!verify.verify(cert.publicKey, signature)) {
+    console.error('Manifest doesn\'t match signature.');
+    process.exit(1);
+  }
+
+  // TODO: Check if the certificate is not expired.
+
+  console.log('Manifest matches signature.');
+/*
+  console.log('Certificate:', cert.publicKey.export({
+    format: 'pem',
+    type: 'pkcs1',
+  }));
+*/
+}
---
+++ src/manifest/manifest-verify.command.ts
@@ -0,0 +1,18 @@
+import { Command } from "commander";
+import { createManifestOption } from "./common";
+import { ManifestVerifyOptions } from "./manifest-verify.options";
+
+export const manifestVerifyCommand = new Command("verify");
+manifestVerifyCommand
+  .summary('Verify manifest file.')
+  .description(
+    'Verify manifest file for correctness and verify it\'s signature.'
+  )
+  .addOption(createManifestOption())
+  .option('-c, --certificate <file>', 'Certificate file.', 'manifest.cert')
+  .option('-s, --signature <file>', 'Signature file (base64 encoded).', 'manifest.sig')
+  .action(async (options: ManifestVerifyOptions) => {
+    const action = await import("./manifest-verify.action");
+    await action.manifestVerifyAction(options);
+  })
+;
