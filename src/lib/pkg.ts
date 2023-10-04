1f314126e8a5d3c7cb83e314785d61fb0fe7fa93
Przemysław Grzywacz <przemyslaw.grzywacz@golem.network>
2023-10-04 13:30:59 +0200

    feat: automatically install dependencies once new project is created JST-453

---
+++ src/lib/pkg.ts
@@ -0,0 +1,37 @@
+export interface PackageManager {
+  name: string;
+  version?: string;
+}
+
+/**
+ * When CLI is run from a package manager, npm_config_user_agent should be set.
+ *
+ * Use this information to determine which package manager is being used.
+ *
+ * @param userAgent Optional user agent.
+ */
+export function getPackageManager(userAgent?: string) {
+  const result: PackageManager = {
+    name: 'npm',
+  }
+
+  const ua = userAgent ?? process.env['npm_config_user_agent'];
+  if (!ua) {
+    return result;
+  }
+
+  const split = ua.split(' ')[0];
+  if (!split) {
+    return result;
+  }
+
+  const [name, version] = split.split('/');
+  if (name) {
+    result.name = name;
+  }
+  if (version) {
+    result.version = version;
+  }
+
+  return result;
+}
